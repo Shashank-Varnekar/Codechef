fb130f0bee34cc62bb7f6935bd5b3e0280cc6a5f
Shashank Varnekar <shashivarnekar@gmail.com>
2021-07-21 16:55:21 +0530

    updated Find_me.cpp

--- Find_Me.cpp
+++ Find_Me.cpp
@@ -3,32 +3,21 @@ using namespace std;
 
 int main()
 {
-    int N, K;
+    int N, K, flag = 0;
     cin >> N >> K;
     int arr[N];
     for(int i=0; i<N; i++){
         cin >> arr[i];
     }
-    int mid = N/2;
-    int left = 0;
-    int right = N-1;
-    int flag = 0;
-    while(right >= left){
-        if(arr[mid] == K){
-            cout << 1 << endl;
+    for(int i=0; i<N; i++){
+        if(arr[i] == K){
             flag = 1;
             break;
         }
-        else if(K > mid){
-            left = mid+1;
-            mid = (left+right)/2;
-        }
-        else{
-            right = mid-1;
-            mid = (left+right)/2;
-        }
     }
-    if(flag == 0)
+    if(flag == 1)
+        cout << 1 << endl;
+    else
         cout << -1 << endl;
     return 0;
 }
